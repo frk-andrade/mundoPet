fafc3db388d28b6c082cdf375689e0317253440a
Davi Silva <dp05799@gmail.com>
2022-05-23 18:14:36 -0300

    fix: arrumando bug caminho no app.js

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users', blogRouter);
+app.use('/blog', blogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/:id', (req, res) => res.send(`POST ${req.params.id}`));
 router.get('/', (req, res) => res.send("Blog"));
 
 
