1b53bda000cd088f9a4decc4a8101193515ea137
Davi Silva <dp05799@gmail.com>
2022-05-25 18:16:14 -0300

    feat: adicionando rota de contato e view contato

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var blogRouter = require('./routes/blog');
 var dogwalkerRouter = require('./routes/dogwalker');
+var contatoRouter = require('./routes/contato');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/blog', blogRouter);
 app.use('/dogwalker', dogwalkerRouter);
+app.use('/contato', contatoRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
