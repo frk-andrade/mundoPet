42c8e9b2d8076f3aa43e6ec8abec31e970551cc2
Davipsilva <dp05799@gmail.com>
2022-06-06 19:50:53 -0300

    feat: adicionando-rotase-views-dogwalker2

--- backend/controller/Dogwalker2Controller.js
+++ backend/controller/Dogwalker2Controller.js
@@ -1,5 +1,5 @@
 const controller = {
-  index: (req, res) => res.render('dogwalker2', { title: "Dogwalker2" }),
+  index: (req, res) => res.render('dogwalker2', { title: "dogwalker2" }),
   show: (req, res) => res.render('post', { title: "Post" })
 }
 
