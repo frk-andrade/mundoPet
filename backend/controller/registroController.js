3cbc6f869d3ce150d1679c368bf3754ee8df3e1d
Franklin <frkdj25@gmail.com>
2022-06-08 18:30:26 -0300

    feature: atualizando as páginas para view ejs e removendo o conteúdo anterior

--- backend/controller/registroController.js
+++ backend/controller/registroController.js
@@ -1,5 +1,5 @@
 const controller = {
-    index: (req, res) = res.render('registro', {title: "Cadastro"})
+    index: (req, res) => res.render('registro', {title: "Cadastro"})
 };
 
 module.exports = controller;
