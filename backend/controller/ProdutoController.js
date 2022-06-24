a2e97dcb345468d69db41d62f19598ed43430755
Franklin <frkdj25@gmail.com>
2022-06-24 17:50:17 -0300

    feature: adicionando controllers

--- backend/controller/ProdutoController.js
+++ backend/controller/ProdutoController.js
@@ -1,5 +1,8 @@
 const controller = {}
 
 controller.index = (req, res) => res.send('Deve abrir a página produto ' + req.params.id)
+controller.criar = (req, res) => res.send('Deve abrir o formulário para adicionar produto ')
+controller.editar = (req, res) => res.send('Deve abrir o formulário preenchido do produto ' + req.params.id)
+
 
 module.exports = controller;
