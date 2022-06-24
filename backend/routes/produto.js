11947b3cbdfd382f7609b37da363d2650ffef184
Franklin <frkdj25@gmail.com>
2022-06-24 17:46:48 -0300

    feature: criando rotas em produtos

--- backend/routes/produto.js
+++ backend/routes/produto.js
@@ -4,5 +4,7 @@ const controller = require('../controller/ProdutoController')
 
 /* GET home page. */
 router.get('/:id', controller.index);
+router.get('/criar', controller.criar);
+router.get('/editar/:id', controller.editar);
 
 module.exports = router;
