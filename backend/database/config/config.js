8c56d916cf1833e79283183d0b34f88098c5346e
Flavio Benedini <flavio.benedini@me.com>
2022-07-01 18:45:15 -0300

    feat: configurando a conexao do banco de dados

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -1,23 +1,10 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+const config = {
+
+  username: 'admin',
+  password: 'admin',
+  database: 'mundopet',
+  host: 'localhost',
+  dialect: 'mysql'
 }
+
+module.exports = config
