495ecd41876ffcf82d9a1f46f09f595b598ae11b
Flavio Benedini <flavio.benedini@me.com>
2022-07-01 19:14:34 -0300

    feat: adicinando a rota /admin/marcas com o formulario funcionando

--- backend/controller/AdminController.js
+++ backend/controller/AdminController.js
@@ -1,9 +1,25 @@
 const controller = {}
 
+const Sequelize = require('sequelize')
+const config = require('../database/config/config.js')
+const db = new Sequelize(config)
+
 controller.index = (req, res) => res.send('teste da rota de admin')
 
 controller.listaMarcas = async (req, res) => {
-    res.render('adicionarmarca', {title: 'Adicionar Marca'})
+    const marcas = await db.query('SELECT * FROM marcas', {type: Sequelize.QueryTypes.SELECT})
+    
+    res.render('listarMarcas', {title: 'Listar Marcas', marcas})
+}
+
+controller.addMarcas = async (req, res) => {
+    const { nome } = req.body
+    const resultado = await db.query(`INSERT INTO marcas (nome) VALUES ('${nome}')`, { type: Sequelize.QueryTypes.INSERT})
+    if (resultado) {
+        res.redirect('/admin/marcas')
+    } else {
+        res.render('error')
+    }
 }
 
 module.exports = controller;
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -5,5 +5,6 @@ const controller = require('../controller/AdminController')
 /* GET home page. */
 router.get('/', controller.index)
 router.get('/marcas', controller.listaMarcas)
+router.post('/marcas', controller.addMarcas)
 
 module.exports = router;
