d5fce7ec0e2fbc0c8d9ebc99ba7f23eaf7d002cb
Flavio Benedini <flavio.benedini@me.com>
2022-07-09 09:58:49 -0300

    feat: ajustando as rotas para usar o controller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,8 @@
+const controller = require('../controller/IndexController')
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', controller.index);
 
 module.exports = router;
