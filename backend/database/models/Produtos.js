368abe45ef9af67d7408a1be86f897991632974f
Flavio Benedini <flavio.benedini@me.com>
2022-07-09 09:08:31 -0300

    criacao do model Produtos.js

---
+++ backend/database/models/Produtos.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataType) => {
+    const Produto = sequelize.define('Produto', {
+      id: {
+        type: DataType.INTEGER.UNSIGNED, // INT UNSIGNED
+        primaryKey: true, // PRIMARY KEY
+        autoIncrement: true, // AUTO_INCREMENT
+        allowNull: false // NOT NULL
+      },
+      nome: {
+        type: DataType.STRING(100),
+        allowNull: false
+      },
+      descricao: {
+        type: DataType.STRING(300),
+        allowNull: false
+      },
+      preco: {
+        type: DataType.DECIMAL(10,2),
+        allowNull: false
+      },
+      promocao: {
+        type: DataType.DECIMAL(10,2),
+        allowNull: true
+      },
+      marca_id: {
+        type: DataType.INTEGER,
+        allowNull: false
+      },
+      categoria_id: {
+        type: DataType.INTEGER,
+        allowNull: false
+      },
+    },
+      {
+        tableName: 'produtos',
+        timestamps: false
+      })
+    return Produto
+    }
