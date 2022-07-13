037944cd6859872403e8b93953399878631f4f18
Franklin Andrade <100174287+frk-andrade@users.noreply.github.com>
2022-07-13 17:55:07 -0300

    Revert "Feature/paginaproduto"

--- backend/controller/ProdutoController.js
+++ backend/controller/ProdutoController.js
@@ -1,14 +1,5 @@
 const controller = {}
-const Sequelize = require('sequelize')
-const config = require('../database/config/config.js')
-const db = new Sequelize(config)
 
-controller.index = async (req, res) => {
-   const {id} = req.params
-   
-   const produto = await db.query(`SELECT * FROM produtos WHERE id = ${id}`, {type: Sequelize.QueryTypes.SELECT})
-//res.send(produto)
+controller.index = (req, res) => res.send('Deve abrir a página produto ' + req.params.id)
 
-res.render('produto', {title: "Produto", produto})
-}
 module.exports = controller;
