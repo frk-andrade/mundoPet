e955872af5123898d708398e133f0ab48fb602ec
Flavio Benedini <flavio.benedini@me.com>
2022-07-15 17:16:00 -0300

    feat: criacao de rotas para os enderecos

--- backend/controller/ContaController.js
+++ backend/controller/ContaController.js
@@ -1,5 +1,31 @@
-const controller = {
-  index: (req, res) => res.render('index', { title: "Index" }),
+const {Categoria, Endereco, Item, Marca, Pedido, Produto, Usuario} = require('../database/models')
+
+
+const controller = {}
+
+
+controller.index = async (req, res) => {
+  
+  // Esse valor do ID vai ser resgatado quando tivermos as sessões funcionando
+  // const { id } = session.id_usuario
+
+  const categorias = await Categoria.findAll()
+  const usuario = await Usuario.findByPk(1)
+  res.render('minha-conta', { title: "Index", categorias, usuario })
 }
+
+controller.showEndereco = async (req, res) => {
+  const { id } = req.params
+  const categorias = await Categoria.findAll()
+  const endereco = await Endereco.findByPk(id)
+
+  // tem que verificar se esse endereço é do usuario que tá pedindo
+  // if (endereco.id_usuario != session.id_usuario)
+  //   res.redirect('/minhaconta')
+  
+  res.render('endereco', { title: "Index", categorias, endereco })
+}
+  
+
 
 module.exports = controller;
