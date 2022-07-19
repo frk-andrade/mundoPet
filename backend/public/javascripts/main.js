97118627f8ad411e01804932d1151cb38506ae00
Franklin <frkdj25@gmail.com>
2022-07-19 18:03:40 -0300

    feature: Atualizando a página minha conta com as validações do formulário, incluindo a do CPF

--- backend/public/javascripts/main.js
+++ backend/public/javascripts/main.js
@@ -1,6 +1,7 @@
 const carousel = new bootstrap.Carousel('#myCarousel')
 
 const myCarouselElement = document.querySelector('#myCarousel')
+
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   wrap: false
