f1782421d1cd6af1f41f3b5f81c64a8bd04cd3a5
Flavio Benedini <flavio.benedini@gmail.com>
2022-07-21 11:52:30 -0300

    feat: criando o migration pedido

---
+++ backend/database/migrations/20220721113323-create-pedido.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pedidos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      usuario_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Usuarios',
+          key: 'id'
+        }
+      },
+      data: {
+        type: Sequelize.DATEONLY
+      },
+      frete: {
+        type: Sequelize.DECIMAL(10, 2)
+      },
+      valor_total: {
+        type: Sequelize.DECIMAL(10, 2)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Pedidos');
+  }
+};
