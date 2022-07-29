e6cccee1da95add61d6d0f53925243681e0c1b18
Flavio Benedini <flavio.benedini@me.com>
2022-07-29 12:59:16 -0300

    feat: ajustes para cadastro e contato

--- backend/controller/IndexController.js
+++ backend/controller/IndexController.js
@@ -1,14 +1,38 @@
 const {Categorias, Endereco, Item, Marca, Pedido, Produto, Usuario} = require('../database/models')
-
-
-
 const controller = {}
 
+controller.index = async (req, res) => {
+  const categorias = await Categorias.findAll()
+  res.render('index', { title: "Index" , categorias})
+}
 
-  controller.index = async (req, res) => {
-    const categorias = await Categorias.findAll()
-    res.render('index', { title: "Index" , categorias})
-  }
+controller.contato = async (req, res) => {
+  const categorias = await Categorias.findAll()
+  res.render('contato', { title: "Contato" , categorias})
+}
+
+controller.sendcontato = async (req, res) => {
+  const categorias = await Categorias.findAll()
+  const {nome, email, mensagem} = req.body
+
+  //enviar um email para o admin do site com o assunto Contato da loja, usando como remetente o email do body e no corpo a mensagem.
+
+  res.render('contato-enviado', { Title: 'Enviado', categorias})
+}
+
+controller.cadastro = async (req, res) => {
+  const categorias = await Categorias.findAll()
+  res.render('cadastro', { title: "Cadastro" , categorias})
+
+}
+
+controller.addCadastro = async (req, res) => {
+  const categorias = await Categorias.findAll()
+  const mensagem = 'Cadastro efetudado com sucesso. Faça seu login.'
+
+  res.render('login', { title: "Login" , categorias, mensagem})
+
+}
 
 
 module.exports = controller;
