74768d22a3af3e9781e986957d5aac1a7b981169
Flavio Benedini <flavio.benedini@me.com>
2022-07-29 09:25:04 -0300

    feat: ajustando rotas e controllers do carrinho

--- backend/controller/CarrinhoController.js
+++ backend/controller/CarrinhoController.js
@@ -1,5 +1,28 @@
-const controller = {
-  index: (req, res) => res.render('index', { title: "Index" }),
+const {Categorias, Endereco, Item, Marca, Pedido, Produto, Usuario} = require('../database/models')
+const controller = {}
+
+//Os itens do carrinho ficarão guardados em um array na sessão
+// var sessao.carrinho = [
+//   {
+//     id_produto: 1,
+//     quantidade: 1
+//   }
+// ]
+
+controller.index = async (req, res) => {
+  categorias = Categorias.findAll()
+
+  res.render('index', { title: "Index", categorias, sessao.carrinho })
+  
 }
+
+contoller.addItem = (req, res) => {
+
+}
+
+contoller.deleteItem = (req, res) => {
+  
+}
+
 
 module.exports = controller;
--- backend/routes/carrinho.js
+++ backend/routes/carrinho.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const controller = require('../controller/CarrinhoController')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+
+router.get('/', controller.index)
+router.post('/', controller.addItem)
+router.delete('/exclui/:id_produto', contoller.deleteItem)
 
 module.exports = router;
