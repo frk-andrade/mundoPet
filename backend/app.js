07c622dd6ebff58b49f3a61110079273f9ece1e8
Flavio Benedini <flavio.benedini@me.com>
2022-07-30 12:39:48 -0300

    fix: ajuste do app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const methodOverride = require('method-override');
+const session = require('express-session')
 
 const indexRouter = require('./routes/index')
 const produtosRouter = require('./routes/produtos')
@@ -12,8 +13,6 @@ const contaRouter = require('./routes/conta')
 const carrinhoRouter = require('./routes/carrinho')
 const adminRouter = require('./routes/admin')
 const loginRouter = require('./routes/login')
-
-
 
 
 const app = express();
@@ -26,6 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'devEngers', resave: false, saveUninitialized: true, cookie: { maxAge: 60000 } }))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
