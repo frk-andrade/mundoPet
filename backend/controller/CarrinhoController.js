08573d9f0dcd0aa5ad44a37d8d6dd819745e3ae5
Franklin <frkdj25@gmail.com>
2022-08-04 18:37:24 -0300

    feature: estilizando o carrinho e inserindo botões na página de produtos

--- backend/controller/CarrinhoController.js
+++ backend/controller/CarrinhoController.js
@@ -3,8 +3,21 @@ const {Categorias, Endereco, Item, Marca, Pedido, Produto, Usuario} = require('.
 const controller = {}
 
 controller.index = async (req, res) => {
-  const categorias = await Categorias.findAll()
-  res.render ("carrinho", {title: "Carrinho", categorias})
+   const {id} = req.params
+   const categorias = await Categorias.findAll()
+   const produto = await Produto.findByPk(id,
+      {
+         include: [
+            {
+              association: 'categoria'
+            },
+            {
+              association: 'marca'
+            }
+          ]
+      
+      })
+  res.render ("carrinho", {title: "Carrinho", categorias, produto})
 }
 
 module.exports = controller;
