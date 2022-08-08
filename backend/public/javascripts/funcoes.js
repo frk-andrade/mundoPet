643ea90e33e5ce562b91ca5df23986cb57796369
Flavio Benedini <flavio.benedini@me.com>
2022-08-07 21:10:03 -0300

    feat: ajustes gerais de produtos e carrinho

--- backend/public/javascripts/funcoes.js
+++ backend/public/javascripts/funcoes.js
@@ -1,10 +1,81 @@
 
-function adicionarCarrinho(id) {
+function adicionarCarrinho(id, nome, preco) {
+  let carrinho = JSON.parse(localStorage.getItem('carrinho'))
+  if(!carrinho) carrinho = []
 
-    console.log({id})
-    const url = '/carrinho'
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(id));
+  const item = {
+    id,
+    nome,
+    quantidade: 1,
+    preco
   }
+
+  if(carrinho.filter(itemFiltrado => itemFiltrado.id === item.id).lenght) {
+    carrinho.map( itemCarrinho => {
+      if(!itemCarrinho.id === item.id) {
+          return itemCarrinho
+      } else {
+          itemCarrinho.quantidade += 1
+          return itemCarrinho
+      }
+    })
+  } else {
+    carrinho.push(item)
+  }
+
+  localStorage.setItem('carrinho', JSON.stringify(carrinho))
+}
+
+function listarCarrinho() {
+
+    const carrinho = JSON.parse(localStorage.getItem('carrinho'))
+
+    if(!carrinho) {
+
+      const mensagem = document.getElementById('mensagem')
+      mensagem.innerHTML('<h2>Carrinho ainda não tem Produtos.</h2>')
+
+    } else {
+
+      
+      const listaCarrinho = document.getElementById('listaCarrinho')
+
+
+
+
+      for (const item of carrinho) {
+        createArticle()
+
+
+        for (const key in item) {
+          const celula = document.createElement('td')
+          const texto = document.createTextNode(item[key])
+          celula.appendChild(texto)
+          linha.appendChild(celula)
+
+        }
+        
+        tbody.appendChild(linha)
+      }
+    }
+}
+
+function alterarQuantidadeCarrinho(id, quantidade) {
+  let carrinho = JSON.parse(localStorage.getItem('carrinho'))
+
+  carrinho.map( itemCarrinho => {
+    if(!itemCarrinho.id === id) {
+        return itemCarrinho
+    } else {
+        itemCarrinho.quantidade = quantidade
+        return itemCarrinho
+    }
+  })
+
+  localStorage.setItem('carrinho', JSON.stringify(carrinho))
+
+}
+
+
+const createArticle = () => createElement('article')
+const createDiv = () => createElement('div')
