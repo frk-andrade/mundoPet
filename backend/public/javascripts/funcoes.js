9ee1f74cd71d90a31fd811b7a5b13b7907359ac5
Flavio Benedini <flavio.benedini@me.com>
2022-08-17 21:13:26 -0300

    feat: deixando o carrinho dinamico

--- backend/public/javascripts/funcoes.js
+++ backend/public/javascripts/funcoes.js
@@ -29,33 +29,97 @@ function adicionarCarrinho(id, nome, preco) {
 function listarCarrinho() {
 
     const carrinho = JSON.parse(localStorage.getItem('carrinho'))
+    const listaCarrinho = document.getElementById('listaCarrinho')
+    const valorTotalCarrinho = document.getElementById('valorTotal')
+
+
+    console.log(carrinho)
 
     if(!carrinho) {
-
+      console.log('entrou')
       const mensagem = document.getElementById('mensagem')
-      mensagem.innerHTML('<h2>Carrinho ainda não tem Produtos.</h2>')
+      mensagem.innerHTML = '<h2 class="titulo-carrinho font-2-m">Carrinho ainda não tem Produtos.</h2>'
+      valorTotalCarrinho.textContent = `Valor Total R$ ${valorDinheiro(0)}`
+
 
     } else {
-
-      
-      const listaCarrinho = document.getElementById('listaCarrinho')
-
+      const valorTotal = carrinho.reduce((total, item) => {
+        return total + (item.preco * item.quantidade)
+      }, 0)
 
 
 
       for (const item of carrinho) {
-        createArticle()
+        const section = document.createElement('section')
+        section.classList.add('card')
 
+        const div = document.createElement('div')
+        div.classList.add('produto-carrinho')
 
-        for (const key in item) {
-          const celula = document.createElement('td')
-          const texto = document.createTextNode(item[key])
-          celula.appendChild(texto)
-          linha.appendChild(celula)
+        const divImgNome = document.createElement('div')
+        const divQtdValor = document.createElement('div')
 
-        }
+        const img = document.createElement('img')
+        img.src = `/images/produtos/produto-25.png`
+        img.alt = `Nome`
+        img.classList.add('img-produto-carrinho')
+
+        const nomeProduto = document.createElement('p')
+        nomeProduto.classList.add('nome-produto-carrinho')
+        nomeProduto.classList.add('font-2-m')
+        nomeProduto.textContent = `${item.nome}`
+
+        const botaoMenos = document.createElement('button')
+        botaoMenos.classList.add('botao-carrinho')
+        botaoMenos.classList.add('botao-menos')
         
-        tbody.appendChild(linha)
+        const spanBotaoMenos = document.createElement('span')
+        spanBotaoMenos.classList.add('material-symbols-outlined')
+        spanBotaoMenos.textContent = 'remove'
+        botaoMenos.appendChild(spanBotaoMenos)
+        botaoMenos.onclick = function () { alteraQtd('-',quantidade.textContent, item.id) }
+        
+        const quantidade = document.createElement('p')
+        quantidade.classList.add('carrinho-qtd')
+        quantidade.textContent = `${item.quantidade}`
+        quantidade.id = 'quantidade'
+        
+        const botaoMais = document.createElement('button')
+        botaoMais.classList.add('botao-carrinho')
+        botaoMais.classList.add('botao-mais')
+        botaoMais.onclick = function() { alteraQtd('+',quantidade.textContent, item.id) }
+
+        const spanBotaoMais = document.createElement('span')
+        spanBotaoMais.classList.add('material-symbols-outlined')
+        spanBotaoMais.textContent = 'add'
+        botaoMais.appendChild(spanBotaoMais)
+
+        const valor = document.createElement('p')
+        valor.classList.add('produto-nome')
+        valor.classList.add('font-2-m')
+        valor.textContent = `R$ ${valorDinheiro(item.preco)}`
+
+        const botaoLixeira = document.createElement('button')
+        botaoLixeira.classList.add('lixeira-carrinho')
+
+        const spanBotaoLixeira = document.createElement('span')
+        spanBotaoLixeira.classList.add('material-symbols-outlined')
+        spanBotaoLixeira.textContent = 'delete'
+        botaoLixeira.appendChild(spanBotaoLixeira)
+
+        divImgNome.appendChild(img)
+        divImgNome.appendChild(nomeProduto)
+        divQtdValor.appendChild(botaoMenos)
+        divQtdValor.appendChild(quantidade)
+        divQtdValor.appendChild(botaoMais)
+        divQtdValor.appendChild(valor)
+        divQtdValor.appendChild(botaoLixeira)
+        div.appendChild(divImgNome)
+        div.appendChild(divQtdValor)
+        section.appendChild(div)
+        listaCarrinho.appendChild(section)
+
+        valorTotalCarrinho.textContent = `Valor Total R$ ${valorDinheiro(valorTotal)}`
       }
     }
 }
@@ -76,6 +140,42 @@ function alterarQuantidadeCarrinho(id, quantidade) {
 
 }
 
+function alteraQtd(operacao, valor, id) {
+    if (operacao === '-'){
+      if(parseInt(valor) > 1) {
+        novoValor = parseInt(valor) - 1
+      } else {
+        novoValor = parseInt(valor)
+      }
+    } else {
+      novoValor = parseInt(valor) + 1
+    }
 
-const createArticle = () => createElement('article')
-const createDiv = () => createElement('div')
+    const carrinho = JSON.parse(localStorage.getItem('carrinho'))
+    quantidade.textContent = novoValor
+
+    novoCarrinho = carrinho.map( itemCarrinho => {
+      if(!itemCarrinho.id === id) {
+          return itemCarrinho
+      } else {
+          itemCarrinho.quantidade = novoValor
+          return itemCarrinho
+      }
+    })
+
+    novoValorTotal = novoCarrinho.reduce((total, item) => {
+      return total + (item.preco * item.quantidade)
+    }
+    , 0)
+
+    const valorTotalCarrinho = document.getElementById('valorTotal')
+    valorTotalCarrinho.textContent = `Valor Total R$ ${valorDinheiro(novoValorTotal)}`
+
+    localStorage.setItem('carrinho', JSON.stringify(novoCarrinho))
+}
+
+function valorDinheiro(valor) {
+  return valor.toFixed(2).replace('.', ',')
+}
+
+
